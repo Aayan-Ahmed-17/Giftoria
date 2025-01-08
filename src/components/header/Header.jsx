72384d268396991ab99ce8676f7e92df52e56085
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-01-08 23:31:27 +0500

    big size navbar done | functionality remaining

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -84,37 +84,48 @@ const Header = () => {
     // </header>
 
     <header className="bg-[#ffbc1f] text-black shadow-lg">
-      <nav className="hidden mx-auto p-4 lg:flex items-center justify-between">
+      <nav className="hidden mx-auto p-4 lg:flex items-center justify-between max-w-[1400px]">
         <div className="logo flex bg-red-100">
           <img src={logo} alt="Logo" className="h-10 mr-2" />
-          <span className="text-3xl font-semibold">Giftoria</span>
+          <span className="text-3xl font-semibold text-purple-900">
+            Giftoria
+          </span>
         </div>
         <ul className="nav-links flex gap-4 text-lg">
           {navLinks.map((list, index) => {
-            return <li key={index}>{list}</li>;
+            return <li key={index}><a href="" className="relative text-white text-lg font-medium transition-all duration-300 after:content-[''] after:block after:w-0 after:h-[1px] after:bg-white after:absolute after:left-0 after:bottom-0 after:transition-all after:duration-300 hover:after:w-full">{list}</a></li>;
           })}
         </ul>
-        <div className="secondary-links flex">
-            {/* <Men */}
+        <div className="secondary-links flex gap-3 text-white">
+          <button>
+            <Search />
+          </button>
+          <button>
+            <ShoppingCart />
+          </button>
         </div>
       </nav>
+
+      {/* mobile Navbar */}
       <nav className="flex px-2 md:px-4 py-4 lg:hidden items-center justify-between">
         <div className="secondary-links flex gap-2">
-            <button><Menu/></button>
-            <button><Search/></button>
+          <button>
+            <Menu />
+          </button>
+          <button>
+            <Search />
+          </button>
         </div>
-        <div className="logo flex">
-        <img
-            src={logo}
-            alt="Logo"
-            className="h-8 mr-1"
-          />
-          <span className={`text-2xl font-semibold text-purple-900`}>Giftoria</span>
+        <div className="logo flex bg-red-100">
+          <img src={logo} alt="Logo" className="h-8 mr-1" />
+          <span className={`text-2xl font-semibold text-purple-900`}>
+            Giftoria
+          </span>
         </div>
         <div className="flex">
-            <button>
-                <ShoppingCart/>
-            </button>
+          <button>
+            <ShoppingCart />
+          </button>
         </div>
       </nav>
     </header>
